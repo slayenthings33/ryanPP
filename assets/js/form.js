75eae32b82424d74acf681c852f6fbe1f6d072e5
Ryan Slayen <ryan_slayen@yahoo.com>
2021-03-08 13:14:54 +0100

    html adjustments

--- assets/js/form.js
+++ assets/js/form.js
@@ -45,7 +45,7 @@
       formData.formDataNameOrder = JSON.stringify(fields);
       formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
       formData.formGoogleSendEmail
-        = form.dataset.email || ""; // no email by default
+        = form.dataset.email || "ryanslayen@gmail.com"; // no email by default
   
       return {data: formData, honeypot: honeypot};
     }
